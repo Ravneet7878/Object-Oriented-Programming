d9f29f6a08215397448bfa200ca1aa5d3a1a741f
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-10-25 18:48:32 +0530

    Add files via upload

---
+++ Inheritance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class rectangle // Parent or Base class
+{
+    public:
+    int length, breadth;
+    
+    public:
+    void display() {cout << "Length = " << length << " " << "Breadth = " << breadth << " ";} 
+    
+    public:
+    void area() {cout << "Area of the rectangle = " << length * breadth << endl;}
+};
+
+class cuboid : public rectangle // Child or Derived class
+{
+    public:
+    int height;
+    
+    public:
+    void show() {cout << "Height = " << height << endl;}
+    
+    public:
+    void volume() {cout << "Volume of the rectangle = " << length * breadth * height << endl;}
+};
+
+int main() 
+{
+	cuboid temp;
+	temp.length = 10, temp.breadth = 20, temp.height = 30;
+	temp.display();
+	temp.show();
+	temp.area();
+	temp.volume();
+	
+    return 0;
+}
