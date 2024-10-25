70efc867aa3dbc0e4c15d0b08b6f354cdde67189
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-10-25 20:44:17 +0530

    Add files via upload

---
+++ Inheritance & Constructors - 2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class base
+{
+    public:
+    base() {cout << "Non Pararmetrized / Default Constructor of the Base Class" << endl;}
+    
+    public:
+    base(int b) {cout << "Pararmetrized Constructor of the Base Class" << " " << b << endl;}
+};
+
+class derived : public base 
+{
+    public:
+    derived() : base() {cout << "Non Pararmetrized / Default Constructor of the Derived Class" << endl;}
+    
+    public:
+    derived(int d) : base(d) {cout << "Pararmetrized Constructor of the Derived Class" << " " << d << endl;}
+};
+
+int main() 
+{
+	derived d1;
+	cout << endl;
+	derived d2(9);
+	
+    return 0;
+}
