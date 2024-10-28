9bd7245580c4e674a0cc6e347f891fc031c19183
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-10-28 18:58:14 +0530

    Add files via upload

---
+++ Function Overriding.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class base
+{
+    public:
+    void msg() {cout << "Base Class" << endl;}
+};
+
+class derived : public base 
+{
+    public:
+    void msg() 
+    {
+        cout << "Derived Class" << endl;
+        base :: msg();
+    }
+};
+
+int main() 
+{
+	derived d;
+	d.msg();
+	
+    return 0;
+}
