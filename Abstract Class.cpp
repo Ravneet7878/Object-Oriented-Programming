50dec8de9e3480e713b92cc1b0b4f6a2590f922f
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-11-01 02:20:05 +0530

    Add files via upload

---
+++ Abstract Class.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Abstract Class is a class that consist of Pure Virtual Function hence this function just makes the class as parent class only which perform no function i.e the object of Parent Class doesn't exist */
+
+class person // Abstract Class
+{
+    public:
+    virtual void give() = 0; // Pure Virtual Function
+};
+
+class boy : public person
+{
+    public:
+    void give() {cout << "Blue Dress" << endl;}
+};
+
+class girl : public person
+{
+    public:
+    void give() {cout << "Pink Dress" << endl;}
+};
+
+int main() 
+{
+    person *ptr = NULL;
+    
+    ptr = new boy();
+    ptr -> give();
+    
+    ptr = new girl();
+    ptr -> give();
+    
+	return 0;
+}
