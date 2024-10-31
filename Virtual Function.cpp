687d0b4c481e3d0285576953263817248d52f33d
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-10-31 22:26:22 +0530

    Add files via upload

---
+++ Virtual Function.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class base
+{
+    public:
+    virtual void msg() {cout << "Base Class" << endl;}
+};
+
+class derived_1 : public base
+{
+    public:
+    void msg() {cout << "Derived_1 Class" << endl;}
+};
+
+class derived_2 : public base
+{
+    public:
+    void msg() {cout << "Derived_2 Class" << endl;}
+};
+
+int main() 
+{
+    base *ptr_1 = new derived_1();
+    ptr_1 -> msg();
+    
+    base *ptr_2 = new derived_2();
+    ptr_2 -> msg();
+    
+	return 0;
+}
