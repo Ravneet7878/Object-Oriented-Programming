9f0ea4fe2f2144311a7e4d77082ec5556f84a953
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-11-02 22:08:31 +0530

    Add files via upload

---
+++ Static Member Function.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Alpha
+{
+    private:
+    int a;
+    int b;
+    
+    public:
+    Alpha() 
+    {
+        a = 5;
+        b = 5;
+    }
+    
+    public:
+    static int count;
+    
+    public:
+    static int get_count()
+    {
+        count++;
+        return count;
+    }
+};
+
+int Alpha :: count = 0;
+
+int main() 
+{
+    Alpha a1;
+    cout << "Value of count using object a1 = " << a1.get_count() << endl;
+    
+    Alpha a2;
+    cout << "Value of count using object a2 = " << a2.get_count() << endl;
+    
+    cout << "Value of count using Alpha = " << Alpha :: get_count() << endl;
+    
+	return 0;
+}
