951974c05a7507fb0c0214da3dd9cdf42bafd1aa
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-11-02 21:03:19 +0530

    Add files via upload

---
+++ Static Member Variable.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Alpha
+{
+    private:
+    int a;
+    int b;
+    
+    public:
+    Alpha() 
+    {
+        a = 5;
+        b = 5;
+        count++;
+    }
+    
+    public:
+    static int count;
+};
+
+int Alpha :: count = 0;
+
+int main() 
+{
+    Alpha a1;
+    Alpha a2;
+    
+    cout << "Value of count using object a1 = " << a1.count << endl;
+    cout << "Value of count using object a2 = " << a2.count << endl;
+    cout << "Value of count using Alpha = " << Alpha :: count << endl;
+    
+	return 0;
+}
