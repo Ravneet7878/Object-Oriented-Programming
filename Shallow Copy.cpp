ed11d40529c44a1e960cf175cb6284a1c87b7b53
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-11-04 00:20:10 +0530

    Add files via upload

---
+++ Shallow Copy.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Dummy
+{
+    private:
+    int a, b;
+    
+    public:
+    void set_data(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+    
+    public:
+    void get_data() {cout << "a = " << a << " & " << "b = " << b << endl;}
+    
+};
+
+int main() 
+{
+	Dummy d1;
+	d1.set_data(5, 6);
+	d1.get_data();
+	
+	Dummy d2 = d1; // Copied using Copy Constructor
+	d2.get_data();
+	
+	Dummy d3;
+	d3 = d1; // Copied using Implicit Copy Assignment Operator
+	d3.get_data();
+	
+	return 0;
+}
