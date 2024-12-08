bdf412aceabdadbf507cfa9de2144afbc9588ca9
Ravneet Singh Oberoi <97469347+Ravneet7878@users.noreply.github.com>
2024-12-08 10:41:31 +0530

    Add files via upload

---
+++ Practice - 4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Output :- 
+ Constructor called
+ Constructor called
+ Destructor called
+ 
+ Explaination :-
+ This is the output because for the object 'a' destructor will be called automatically once the main function is executed completely as it is static memory allocation which is stored using stack whereas for the object 'a' we have to call destructor using delete[] as it is dynamic memory allocation which stored using heap.
+*/
+
+class person
+{
+    public:
+    person() {cout << "Constructor called" << endl;}
+    
+    public:
+    ~person() {cout << "Destructor called" << endl;}
+};
+
+int main() 
+{
+	person a;
+	person *b = new person();
+	
+	return 0;
+}
